2808a1b872dd402a61c0fef67fc5b09b01c3d15b
Manish Sharma <code.manish94@gmail.com>
2025-09-13 13:25:09 +0530

    feat(api): added support to validate oas

--- api/src/main/java/io/github/sharma_manish_94/schemasculpt_api/controller/ValidationController.java
+++ api/src/main/java/io/github/sharma_manish_94/schemasculpt_api/controller/ValidationController.java
@@ -1,16 +1,30 @@
 package io.github.sharma_manish_94.schemasculpt_api.controller;
 
+import io.github.sharma_manish_94.schemasculpt_api.dto.ValidationError;
+import io.github.sharma_manish_94.schemasculpt_api.dto.ValidationRequest;
+import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ValidationController {
 	
 	@GetMapping("/health")
 	public ResponseEntity<String> healthCheck() {
 		return ResponseEntity.ok("Validation API is running");
+	}
+	
+	@PostMapping("/validate")
+	public ResponseEntity<List<ValidationError>> validateSpecification(
+			@RequestBody ValidationRequest request
+	) {
+		SwaggerParseResult result = new OpenAPIV3Parser().readContents(request.spec());
+		final List<ValidationError> errors = result.getMessages().stream().map(ValidationError::new).toList();
+		return ResponseEntity.ok(errors);
 	}
 }
