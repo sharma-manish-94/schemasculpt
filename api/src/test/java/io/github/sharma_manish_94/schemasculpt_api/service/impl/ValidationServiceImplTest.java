e1110b6d651ef383793edc743e6801068c35e606
Manish Sharma <code.manish94@gmail.com>
2025-09-13 15:24:06 +0530

    added test cases for validation service

---
+++ api/src/test/java/io/github/sharma_manish_94/schemasculpt_api/service/impl/ValidationServiceImplTest.java
@@ -0,0 +1,12 @@
+package io.github.sharma_manish_94.schemasculpt_api.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationServiceImplTest {
+	
+	@Test
+	void analyze() {
+	}
+}
